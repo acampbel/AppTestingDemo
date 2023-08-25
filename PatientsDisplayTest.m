10bdf6782947538a54b44aa98411658759b345f5
Andy Campbell <47190204+acampbel@users.noreply.github.com>
2023-08-25 14:47:34 -0400

    Update PatientsDisplayTest.m

--- PatientsDisplayTest.m
+++ PatientsDisplayTest.m
@@ -6,10 +6,12 @@ classdef PatientsDisplayTest < matlab.uitest.TestCase
     methods (TestMethodSetup)
         function launchApp(testCase)
             testCase.App = PatientsDisplay;
+            testCase.logScreenshot("Launch");
             testCase.addTeardown(@delete,testCase.App)
+            testCase.addTeardown(@logScreenshot, testCase, "Teardown");
         end
         function takeScreenshotOnFailure(testCase)
-            testCase.onFailure(matlab.unittest.diagnostics.ScreenshotDiagnostic)
+            testCase.onFailure(ScreenshotDiagnostic)
         end
     end
 
@@ -17,7 +19,9 @@ classdef PatientsDisplayTest < matlab.uitest.TestCase
         function testTab(testCase)
             % Choose the Data tab
             dataTab = testCase.App.DataTab;
+            testCase.logScreenshot("testTab-choose-before");
             testCase.choose(dataTab)
+            testCase.logScreenshot("testTab-choose-before");
 
             % Verify that the tab has the expected title
             testCase.verifyEqual( ...
@@ -26,14 +30,18 @@ classdef PatientsDisplayTest < matlab.uitest.TestCase
 
         function testPlottingOptions(testCase)
             % Press the Histogram radio button
+            testCase.logScreenshot("testPlottingOptions-press-histo-before");
             testCase.press(testCase.App.HistogramButton)
+            testCase.logScreenshot("testPlottingOptions-press-histo-after");
 
             % Verify that the x-axis label changed from Weight to Systolic
             testCase.verifyEqual(testCase.App.UIAxes.XLabel.String, ...
                 'Systolic')
 
             % Change the bin width to 9
+            testCase.logScreenshot("testPlottingOptions-choose-bin-width-before");
             testCase.choose(testCase.App.BinWidthSlider,9)
+            testCase.logScreenshot("testPlottingOptions-choose-bin-width-after");
 
             % Verify the number of bins
             testCase.verifyEqual(testCase.App.UIAxes.Children.NumBins,4)
@@ -54,7 +62,9 @@ classdef PatientsDisplayTest < matlab.uitest.TestCase
             testCase.verifyEqual(ax.Children.YData,maleSystolicData)
 
             % Switch to Diastolic readings
+            testCase.logScreenshot("testBloodPressure-choose-diastolic-before");
             testCase.choose(testCase.App.BloodPressureSwitch,'Diastolic')
+            testCase.logScreenshot("testBloodPressure-choose-diastolic-after");
 
             % Verify the y-axis label and that the male Diastolic data
             % is displayed
@@ -69,7 +79,9 @@ classdef PatientsDisplayTest < matlab.uitest.TestCase
             testCase.verifyNumElements(ax.Children.XData,47)
 
             % Include the female data
+            testCase.logScreenshot("testGender-include-female-before");
             testCase.choose(testCase.App.FemaleCheckBox)
+            testCase.logScreenshot("testGender-include-female-after");
 
             % Verify the number of displayed data sets and the color
             % representing the female data
@@ -77,12 +89,30 @@ classdef PatientsDisplayTest < matlab.uitest.TestCase
             testCase.verifyEqual(ax.Children(1).CData,[1 0 0])
 
             % Exclude the male data
+            testCase.logScreenshot("testGender-exclude-male-before");
             testCase.choose(testCase.App.MaleCheckBox,false)
-
+            testCase.logScreenshot("testGender-exclude-male-after");
+            
             % Verify the number of displayed data sets and the number of
             % scatter points
             testCase.verifyNumElements(ax.Children,1)
             testCase.verifyNumElements(ax.Children.XData,53)
         end
     end
+    methods(Access=private)
+        function logScreenshot(testCase, prefix)
+            args = {};
+            if nargin > 1
+                args = {"Prefix", prefix};
+            end
+            testCase.log(1, ScreenshotDiagnostic, args{:});
+        end
+    end
+
+end
+
+
+
+function d = ScreenshotDiagnostic(varargin)
+d = matlab.unittest.diagnostics.ScreenshotDiagnostic(varargin{:});
 end
