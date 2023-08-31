f79a828ecc8ab3bb179b51467246de271e860e5a
acampbel <acampbel@mathworks.com>
2023-08-30 20:37:07 -0400

    Unembed images for md

--- buildfile.m
+++ buildfile.m
@@ -17,22 +17,21 @@ end
 function markdownTask(ctx)
 % Generate markdown from all mlx files
 
-exportHelper(ctx, "markdown");
+mlxFiles = ctx.Task.Inputs.paths;
+mdFiles = ctx.Task.Outputs.paths;
+for idx = 1:numel(mlxFiles) 
+    disp("Building markdown file from " + mlxFiles(idx))
+    export(mlxFiles(idx), mdFiles(idx), Run=true, EmbedImages=false);
+end
 end
 
 function jupyterTask(ctx)
-% Generate jupyter notebooks from all mlx files
-
-exportHelper(ctx, "jupyter");
-end
-
-function exportHelper(ctx, type)
-% Generate markdown from all mlx files
+% Generate jupyer notebooks from all mlx files
 
 mlxFiles = ctx.Task.Inputs.paths;
-outFiles = ctx.Task.Outputs.paths;
+ipynbFiles = ctx.Task.Outputs.paths;
 for idx = 1:numel(mlxFiles) 
-    disp("Building "+ type + " file from " + mlxFiles(idx))
-    export(mlxFiles(idx), outFiles(idx), Run=true);
+    disp("Building jupyter notebook from " + mlxFiles(idx))
+    export(mlxFiles(idx), ipynbFiles(idx), Run=true);
 end
 end
